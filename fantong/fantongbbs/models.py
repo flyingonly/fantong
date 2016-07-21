8c6730dfa0c99fc93947b1456863cfbb4f163cff
flyingonly <wangwy14@yeah.net>
2016-07-21 20:45:54 +0800

    models改动

--- fantong/fantongbbs/models.py
+++ fantong/fantongbbs/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class User(models.Model):
 	UAccount = models.EmailField()
-	UName = models.CharField(maxlength = 50)
-	UPassword = models.CharField(maxlength = 50)
+	UName = models.CharField(max_length = 50)
+	UPassword = models.CharField(max_length = 50)
 	UImage = models.ImageField()
 	UAdmin = models.BooleanField()
 	UFollowUserNum = models.IntegerField()
@@ -16,16 +16,16 @@ class User(models.Model):
 	def __str__(self):
 		return self.UName
 
-class Post(models.Model)
+class Post(models.Model):
 	PUserID = models.ForeignKey(User)#帖子作者关系
-	PTitle = models.CharField(maxlength = 100)
+	PTitle = models.CharField(max_length = 100)
 	PContent = models.TextField()
 	PTime = models.DateTimeField()
 	PLastComTime = models.DateTimeField()
-	PTagLocation = models.CharField(maxlength = 50)
-	PTagClass = models.CharField(maxlength = 50)
-	PTagPrice = models.CharField(maxlength = 50)
-	PKeywords = models.CharField(maxlength = 100)
+	PTagLocation = models.CharField(max_length = 50)
+	PTagClass = models.CharField(max_length = 50)
+	PTagPrice = models.CharField(max_length = 50)
+	PKeywords = models.CharField(max_length = 100)
 	PDelete = models.BooleanField()
 	PLikeNum = models.IntegerField()
 	PSection = models.IntegerField()
@@ -37,19 +37,19 @@ class Post(models.Model)
 	def __str__(self):
 		return self.PTitle
 
-class FollowUser(models.Model)
+class FollowUser(models.Model):
 	User1ID = models.ForeignKey(User)
 	User2ID = models.ForeignKey(User)
 
-class UserReportPost(models.Model)
+class UserReportPost(models.Model):
 	UserID = models.ForeignKey(User)
 	PostID = models.ForeignKey(Post)
 	ReportContent = models.TextField()
 
-class UserLikePost(models.Model)
+class UserLikePost(models.Model):
 	UserID = models.ForeignKey(User)
 	PostID = models.ForeignKey(Post)
 
-class UserFollowPost(models.Model)
-	UserID = ForeignKey(User)
-	PostID = ForeignKey(Post)
+class UserFollowPost(models.Model):
+	UserID = models.ForeignKey(User)
+	PostID = models.ForeignKey(Post)
