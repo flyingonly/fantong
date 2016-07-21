480b920b6cd40410285daf50dd0e3e0e495919c7
flyingonly <wangwy14@yeah.net>
2016-07-22 03:32:13 +0800

    第一版主页可运行状态

--- fantong/fantong/settings.py
+++ fantong/fantong/settings.py
@@ -121,8 +121,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'static/',
+]
 
 MEDIA_ROOT = 'media/'
 MEDIA_URL = 'media/'
