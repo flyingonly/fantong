6c0ea0f7abedc39302ddd257e130228ef75eb158
flyingonly <wangwy14@yeah.net>
2016-07-23 00:47:09 +0800

    修改密码

--- fantong/fantongbbs/forms.py
+++ fantong/fantongbbs/forms.py
@@ -1,13 +1,24 @@
 from django import forms
-from .models import BBSPost,BBSUser
+from .models import BBSPost, BBSUser
 from django.contrib.auth.models import User
+
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = BBSPost
         fields = ('PTitle', 'PContent')
 
+
 class ImageForm(forms.ModelForm):
     class Meta:
         model = BBSUser
         fields = ('UFollowUserNum',)
+
+
+class ChangepwdForm(forms.Form):
+    old_pwd = forms.CharField(label='OLD PASSWORD', widget=forms.PasswordInput)
+    new_pwd = forms.CharField(label='NEW PASSWORD', widget=forms.PasswordInput)
+    new_pwd2 = forms.CharField(label='CONFIRM', widget=forms.PasswordInput)
+
+    def pwd_validate(self, p1, p2):
+        return p1 == p2
--- fantong/fantongbbs/urls.py
+++ fantong/fantongbbs/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.bbs_list, name='index'),
+    url(r'^personal/', views.get_user),
+    url(r'changepassword/(.+)/$', views.change_password),
 ]
