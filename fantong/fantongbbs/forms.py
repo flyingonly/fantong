18920f1eb4bcaf5ae20a64cedc388a9a961e0ee7
flyingonly <wangwy14@yeah.net>
2016-07-22 16:36:14 +0800

    回帖功能

--- fantong/fantongbbs/forms.py
+++ fantong/fantongbbs/forms.py
@@ -1,24 +1,8 @@
 from django import forms
-from .models import Post
-from django.contrib.auth.models import User
-from registration.forms import RegistrationForm
+from .models import BBSPost
 
-
-class RegistrationForm(RegistrationForm):
-    UFollowUserNum = forms.IntegerField()
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2", "UFollowUserNum")
-
-    def save(self, commit=True):
-        user = super(RegistrationForm, self).save(commit=False)
-        user.BBSUser.UFollowUserNum = self.cleaned_data["UFollowUserNum"]
-        if commit:
-            user.save()
-        return user
 
 class PostForm(forms.ModelForm):
-	class Meta:
-		model = Post
-		fields = ('title', 'content')
+    class Meta:
+        model = BBSPost
+        fields = ('PTitle', 'PContent')
