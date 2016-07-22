fdbad21424aaa2e1d9a50ac7b191d5414d7268f0
flyingonly <wangwy14@yeah.net>
2016-07-22 16:15:09 +0800

    123

--- fantong/fantongbbs/views.py
+++ fantong/fantongbbs/views.py
@@ -3,12 +3,12 @@ from .models import BBSPost
 
 
 def bbs_list(request):
-	params = request.POST if request.method == 'POST' else None
-	form = PostForm(params)
-	if form.is_valid():
-		post - form.save(commit=False)
-		post.author = request.user
-		post.save()
-		form = PostForm()
-	posts = BBSPost.objects.all()
-	return render(request, 'index.html', {'posts': posts})
+    params = request.POST if request.method == 'POST' else None
+    form = PostForm(params)
+    if form.is_valid():
+	    post - form.save(commit=False)
+	    post.author = request.user
+	    post.save()
+	    form = PostForm()
+    posts = BBSPost.objects.all()
+    return render(request, 'index.html', {'posts': posts})
