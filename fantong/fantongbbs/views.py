2c513aaa7b94411453734e90270a27511a363c82
unknown <>
2016-07-22 16:02:08 +0800

    Reply completed

--- fantong/fantongbbs/views.py
+++ fantong/fantongbbs/views.py
@@ -3,5 +3,12 @@ from .models import BBSPost
 
 
 def bbs_list(request):
-    posts = BBSPost.objects.all()
+    params = request.POST if request.method == 'POST' else None
+	form = PostForm(params)
+	if form.is_valid():
+		post - form.save(commit=False)
+		post.author = request.user
+		post.save()
+		form = PostForm()
+	posts = BBSPost.objects.all()
     return render(request, 'index.html', {'posts': posts})
