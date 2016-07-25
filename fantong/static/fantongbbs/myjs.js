6ae7fed68356fdc555f879afa09a1a87b931cf67
flyingonly <wangwy14@yeah.net>
2016-07-25 20:10:13 +0800

    改动未完成

--- fantong/static/fantongbbs/myjs.js
+++ fantong/static/fantongbbs/myjs.js
@@ -3,14 +3,32 @@ $(function() {
         var pid = $(this).attr("postid");
         var content = $('form.post-form[postid=' + pid + '] textarea').val();
         if (content != '') {
-            $.ajax({ url: '/ajax_deal/',
+            $.ajax({
+                url: '/ajax_deal/',
                 type: 'post',
                 data: { 'PParentID': pid, 'PContent': content },
-                success: function(data, textStatus){
+                success: function(data, textStatus) {
                     window.location.reload()
-            }
+                }
             })
         }
     })
-    return true;
+    $('#fileImage').bind("change", function() {
+        if ($(this).prop('files')[0].type.indexOf("image") <= -1) {
+            alert("请选择图片！");
+            return;
+        }
+        var formdata = new FormData()
+        formdata.append("file",$(this).prop('files')[0])
+        $.ajax({
+            url: "/ajax_append_image/",
+            type: "POST",
+            data: formdata,
+            processData: false,
+            contentType: false
+        });
+        console.log($(this).prop('files')[0].name);
+        $("form textarea").append("<img src='" + $(this).val() + "'>")
+        alert("{%static asd %}")
+    })
 });
